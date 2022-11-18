ddfd55042025d49e1d684490fb6f4816672a92c6
regadior <regadior@gmail.com>
2022-11-18 19:19:09 +0100

    Login estructura inicial completo (Añadir efectos->)

--- dist/script.js
+++ dist/script.js
@@ -1,15 +1,15 @@
-var x=document.getElementById("login");
-			var y=document.getElementById("register");
-			var z=document.getElementById("btn");
-			
-			function register() {
-				x.style.left = "-200px";
-				y.style.left = "190px";
-				z.style.left = "110px";
-			}
-			
-			function login() {
-				x.style.left = "190px";
-				y.style.left = "-500px";
-				z.style.left = "0";
-			}
+let x = document.getElementById("login");
+let y = document.getElementById("register");
+let z = document.getElementById("boton-absoluto");
+
+function register() {
+	x.style.left = "-400px";
+	y.style.left = "190px";
+	z.style.left = "184px";
+}
+
+function login() {
+	x.style.left = "190px";
+	y.style.left = "-500px";
+	z.style.left = "0";
+}
